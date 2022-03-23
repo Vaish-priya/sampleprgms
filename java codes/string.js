bd4475268333b8734bbb4d76949abb42a82db5e8
Vaish-priya <101140460+Vaish-priya@users.noreply.github.com>
2022-03-23 11:51:50 +0530

    Add files via upload

---
+++ java codes/string.js
@@ -0,0 +1,7 @@
+var str1 = 'Hello';
+var str2 = 'everyone';
+console.log("Combined are: " + str1.concat(str2));
+console.log("Character At 5: " + str2.charAt(5));
+console.log("Index of o: " + str2.indexOf('o'));
+console.log("After Replacement: " + str2.replace('everyone', 'fellowones'));
+console.log("UpperCase: " + str2.toUpperCase());
