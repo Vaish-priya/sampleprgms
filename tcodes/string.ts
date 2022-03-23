42d047d7ad074fab32ef5c04953075023c5eaa0e
Vaish-priya <101140460+Vaish-priya@users.noreply.github.com>
2022-03-23 11:41:14 +0530

    Add files via upload

---
+++ tcodes/string.ts
@@ -0,0 +1,12 @@
+let str1: string = 'Hello';  
+let str2: string = 'everyone';  
+ 
+console.log("Combined are: " +str1.concat(str2));  
+ 
+console.log("Character At 5: " +str2.charAt(5));  
+  
+console.log("Index of o: " +str2.indexOf('o'));  
+  
+console.log("After Replacement: " +str2.replace('everyone', 'fellowones'));  
+  
+console.log("UpperCase: " +str2.toUpperCase());  
